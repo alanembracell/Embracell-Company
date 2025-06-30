b6a2ccf535331b641af3d2fe5a681ed36ae35cec
alanembracell <alanworkcenter@gmail.com>
2025-06-30 11:02:11 -0300

    Add files via upload

--- os.js
+++ os.js
@@ -1,34 +1,16 @@
 
-function autenticar() {
-  const senha = document.getElementById('senha').value;
-  if (senha === '1234') {
-    document.getElementById('painel').style.display = 'block';
-  } else {
-    alert('Senha incorreta!');
-  }
-}
-
-function gerarOS() {
-  const cliente = document.getElementById('cliente').value;
-  const descricao = document.getElementById('descricao').value;
-  const status = document.getElementById('status').value;
-  const codigo = Math.floor(Math.random() * 100000).toString();
-  const os = { cliente, descricao, status, codigo };
-  localStorage.setItem('OS_' + codigo, JSON.stringify(os));
-  document.getElementById('osGerada').innerText = 'OS gerada com sucesso! Código: ' + codigo;
-}
-
-function buscarOS() {
-  const codigo = document.getElementById('osBusca').value;
+function consultarOS() {
+  const codigo = document.getElementById('codigo').value.trim();
   const osData = localStorage.getItem('OS_' + codigo);
+  const output = document.getElementById('resultadoConsulta');
   if (osData) {
     const os = JSON.parse(osData);
-    document.getElementById('resultadoConsulta').innerHTML = `
+    output.innerHTML = `
       <p><strong>Cliente:</strong> ${os.cliente}</p>
       <p><strong>Problema:</strong> ${os.descricao}</p>
       <p><strong>Status:</strong> ${os.status}</p>
     `;
   } else {
-    document.getElementById('resultadoConsulta').innerText = 'OS não encontrada.';
+    output.innerText = 'Ordem de Serviço não encontrada.';
   }
 }
